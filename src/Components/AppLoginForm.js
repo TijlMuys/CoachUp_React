e83fc7395cb46f300b3f42be27155aba18c6d037
Tijl Muys <tijl.muys@student.ehb.be>
2019-04-16 13:30:12 +0200

    recovered loginform

--- src/Components/AppLoginForm.js
+++ src/Components/AppLoginForm.js
@@ -1,42 +1,79 @@
-import React, { Component } from 'react';
-import {Form} from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Form, FormGroup, FormControl} from 'react-bootstrap';
 import {Button} from 'react-bootstrap';
-import {Card} from 'react-bootstrap';
-import {Container} from 'react-bootstrap';
-import {Row} from 'react-bootstrap';
-import {Col} from 'react-bootstrap';
+import AppRegisterForm from "./AppRegisterForm";
+
 
 class AppLoginForm extends Component {
-    render() {
+
+    state = {
+        showRegister: false
+    };
+
+    login = () => {
+        window.localStorage.setItem("authToken", "dummyToken");
+        this.props.loginHandler();
+    };
+
+    formChange = (event) =>
+    {
+        let loginObject = {...this.state};
+        loginObject[event.target.name] = event.target.value;
+        this.setState(loginObject);
+    }
+
+    toggleFormType = () =>
+    {
+        if (this.state.showRegister) {
+            this.setState({showRegister: false});
+        }
+        else {
+            this.setState({showRegister: true});
+        }
+
+    }
+
+    renderForm = () =>
+    {
+        if (this.state.showRegister === false) {
+            return (
+            <div className = "Login" style = {{'padding': '10vh 10vw'}}>
+                <div className = "text-center" >
+                    <img className = "mb-4" style = {{'margin': '0 auto'}} src = {require("../images/logo.png")} alt = "CoachUp Logo" width = "85vw" height = "85vh" />
+                </div>
+                < h1 className = "text-center h4 mb-3 font-weight-normal" style = {{'margin': '0 auto'}}>Please sign in</h1>
+                <Form style = {{'margin': '0 auto', 'maxWidth': '320px'}}>
+                    <FormGroup controlId = "email" >
+                        <Form.Label>Email</Form.Label>
+                        <FormControl type = "email" name = "email" onChange = {this.formChange}/>
+                    </FormGroup>
+                    <FormGroup controlId = "password" >
+                    <Form.Label> Password </Form.Label>
+                    <FormControl type = "password" name = "password" onChange = {this.formChange}/>
+                    </FormGroup>
+                    <Button onClick = {this.login} block>
+                        Login
+                    </Button>
+                    <hr/>
+                    <Button variant = "outline-primary" block onClick = {this.toggleFormType}>
+                        Register
+                    </Button>
+                </Form>
+            </div>
+        )
+            ;
+        }
+        else {
+            return (
+                < AppRegisterForm toggleFormType = {this.toggleFormType}/>
+            );
+        }
+    }
+
+    render()
+    {
         return (
-            <Container style={{paddingTop: '1rem'}}>
-                <Row>
-                    <Col>
-                        <Card style={{padding: '1rem'}}>
-                            <Card.Title>Login</Card.Title>
-                            <Form>
-                                <Form.Group controlId="formBasicEmail">
-                                    <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="Enter email" style={{maxWidth: '12rem'}} />
-                                    {/*<Form.Text className="text-muted">
-                                        We'll never share your email with anyone else.
-                                    </Form.Text>*/}
-                                </Form.Group>
-                                <Form.Group controlId="formBasicPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" placeholder="Password" style={{maxWidth: '12rem'}} />
-                                </Form.Group>
-                                {/*<Form.Group controlId="formBasicChecbox">
-                                    <Form.Check type="checkbox" label="Check me out" />
-                                </Form.Group>*/}
-                                <Button variant="primary" type="submit">
-                                    Submit
-                                </Button>
-                            </Form>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+            <div>{this.renderForm()}</div>
         );
     }
 }
